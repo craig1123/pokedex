eb4de0676d89fe436c80fa20fb9fafcee43a2a41
craig1123 <craigwalker1123@gmail.com>
2016-07-13 17:03:07 -0600

    api works

--- js/pokeCtrl.js
+++ js/pokeCtrl.js
@@ -1,8 +1,12 @@
 angular.module('pokedex')
   .controller('pokeCtrl', function ($scope, pokeServ) {
 
-//manipulate api data here
-
-
+    $scope.pokeData = function (num) {
+      pokeServ.getPokemon(num).then(function (response) {
+        console.log(response);
+        $scope.pokeName = response.name;
+        $scope.pokepic = response.sprites.front_shiny;
+      });
+    };
 
   });
--- js/pokeServ.js
+++ js/pokeServ.js
@@ -1,7 +1,14 @@
 angular.module('pokedex')
-  .service('pokeServ', function () {
-    //make api call to 
-
+  .service('pokeServ', function ($http) {
+    console.log('service');
+    this.getPokemon = function (num) {
+      return $http({
+        method: 'GET',
+        url: 'http://pokeapi.co/api/v2/pokemon/' + num + '/'
+      }).then(function (response) {
+        return response.data
+      })
+    }
 
 
   });
