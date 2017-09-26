75437d65f94bdcdd6bfe17f7f7dd7b712a795bb7
Craig Walker <cwalker@overstock.com>
2017-09-25 21:32:10 -0600

    getting the correct file path

--- js/routes.js
+++ js/routes.js
@@ -3,40 +3,40 @@ angular.module('pokedex')
                   $stateProvider
                       .state('home', {
                         url: '/',
-                        templateUrl: '../views/home.html',
+                        templateUrl: './views/home.html',
                         controller: 'mainCtrl',
                         controllerAs: 'vm'
                       })
                       .state('mainSearch', {
                         url: '/pokeSearch',
-                        templateUrl: '../views/mainSearch.html',
+                        templateUrl: './views/mainSearch.html',
                         controller: 'mainCtrl',
                         controllerAs: 'vm'
                       })
                       .state('randomSearch', {
                         url: '/randomizer',
-                        templateUrl: '../views/randomizer.html',
+                        templateUrl: './views/randomizer.html',
                         controller: 'mainCtrl',
                         controllerAs: 'vm'
                       })
                       .state('details', {
                         url: '/details',
-                        templateUrl: '../views/details.html',
+                        templateUrl: './views/details.html',
                         controller: 'mainCtrl',
                         controllerAs: 'vm'
                       })
                       .state('prize', {
                         url: '/easteregg',
-                        templateUrl: '../views/prize.html',
+                        templateUrl: './views/prize.html',
                       })
                       .state('regions', {
                         url: '/regions',
-                        templateUrl: '../views/regions.html',
+                        templateUrl: './views/regions.html',
                         controller: 'mainCtrl'
                       })
                       .state('types', {
                         url: '/types',
-                        templateUrl: '../views/types.html',
+                        templateUrl: './views/types.html',
                         controller: 'mainCtrl',
                         controllerAs: 'vm'
                       });
