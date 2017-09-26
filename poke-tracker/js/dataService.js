8beb67647e4a20513259158859b5b25e430e9cea
Craig Walker <cwalker@overstock.com>
2017-09-25 19:14:04 -0600

    adding https to everything

--- poke-tracker/js/dataService.js
+++ poke-tracker/js/dataService.js
@@ -5,13 +5,13 @@ angular
     this.getPokemon = function() {
       return $http({
         method: 'GET',
-        url: 'http://pokeapi.co/api/v2/pokemon/?limit=811'
+        url: 'https://pokeapi.co/api/v2/pokemon/?limit=811'
       }).then(function(response) {
         var data = response.data.results;
         var pokemon = [];
         for (var i = 0; i < data.length; i++) {
           if (i < 721) {
-            pokemon.push({name: data[i].name.charAt(0).toUpperCase() + data[i].name.slice(1), id: '#' + (i + 1), img: 'http://www.serebii.net/art/th/' + (i + 1) + '.png'});
+            pokemon.push({name: data[i].name.charAt(0).toUpperCase() + data[i].name.slice(1), id: '#' + (i + 1), img: 'https://www.serebii.net/art/th/' + (i + 1) + '.png'});
           }
           else {
             if (data[i].name.includes('pikachu-') || data[i].name.includes('-eternal') || data[i].name.includes('-small') || data[i].name.includes('-large') || data[i].name.includes('-super') || data[i].name.includes('-sunny') || data[i].name.includes('-snowy') || data[i].name.includes('-rainy') || data[i].name.includes('-striped')){
