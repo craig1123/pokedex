aad1efcc867944f7f5f66ff4cf868bdebf7c74cd
Craig Walker <craigwalker1123@gmail.com>
2017-11-14 21:50:08 -0700

    adding a lazy scroller

--- poke-tracker/js/trackerCtrl.js
+++ poke-tracker/js/trackerCtrl.js
@@ -1,20 +1,27 @@
-angular
-.module('tracker')
-.controller('trackerCtrl', function($scope, dataService) {
+angular.module('tracker').controller('trackerCtrl', function($scope, dataService) {
 
   $scope.delay = true;
   $scope.getPokemon = function() {
     dataService.getPokemon().then(function(pokemon) {
-      $scope.pokemons = pokemon;
+      $scope.pokes = pokemon;
       $scope.delay = false;
+      $scope.loadImages();
     });
   };
+
+  $scope.pokemons = [];
+  $scope.counter = 24;
+  $scope.loadImages = function() {
+      $scope.pokemons = $scope.pokes.slice(0, $scope.counter)
+      $scope.counter += 24;
+  };
+
 
   $scope.fav = false;
   $scope.getPokemon();
   $scope.flipCard = false;
-})
-.filter('filter', function() {
+
+}).filter('filter', function() {
   return function(pokemon, search) {
       if (search.id || search.name) {
         var results = [];
@@ -32,4 +39,18 @@ angular
       return pokemon;
     }
   };
-});;  //End mainCtrl
+});
+
+angular.module('tracker').directive('whenScrolled', function() {
+    return function(scope, elm, attr) {
+        var raw = elm[0];
+        elm.on('scroll', function() {
+            if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
+                scope.$apply(attr.whenScrolled);
+            }
+        });
+    };
+});
+
+
+
